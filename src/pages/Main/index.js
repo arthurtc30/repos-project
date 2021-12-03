919d81049e5d2f0f8eb0e03f03b37bfa81ea543b
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2021-12-03 12:19:47 -0300

    main style

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Title } from "./styles";
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from "./styles";
 
 export default function Main() {
   return (
-    <Title>
-      Main
-      <span>Oi</span>
-    </Title>
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        My repositories
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Add repos" ></input>
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={25} />
+        </SubmitButton>    
+      </Form>
+    </Container>
   );
 }
