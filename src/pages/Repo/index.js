7b03aae21aade4fccb8baeee86c36712f8a871e5
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2021-12-03 16:32:49 -0300

    repo open issues list

--- src/pages/Repo/index.js
+++ src/pages/Repo/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Owner, Loading, BackButton } from './styles';
+import { Container, Owner, Loading, BackButton, IssuesList } from './styles';
 import api from '../../services/api';
 import { FaArrowLeft } from 'react-icons/fa';
 
@@ -43,11 +43,35 @@ export default function Repo({ match }) {
       <BackButton to="/">
         <FaArrowLeft color="#0D2636" size={35} />
       </BackButton>
+      
       <Owner>
         <img src={repo.owner.avatar_url} alt={repo.owner.login} />
         <h1>{repo.name}</h1>
         <p>{repo.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>
+                  {issue.title}
+                </a>
+
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>
+                    {label.name}
+                  </span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   );
 }
